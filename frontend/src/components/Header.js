518530c009ac1aa2a06a879bd051f953dcc638b5
Mrinmayee Rane <mrinmayeerane2810@gmail.com>
2025-02-23 11:06:12 -0500

    removed the header from login and signup page

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -7,30 +7,32 @@ function Header({ email }) {
   const navigate = useNavigate();
   return (
     <>
-      <header className={hstyles.header}>
-        <div>
-          <img className={styles.filtergreen} src="/logo.svg" alt="logo" />
-        </div>
-        <div className="flex space-x-4">
-          <span>{email}</span>
-          <button
-            className={hstyles.button}
-            onClick={() => {
-              navigate("/journal");
-            }}
-          >
-            New Entry
-          </button>
-          <button
-            className={hstyles.button}
-            onClick={() => {
-              navigate("/view-journal");
-            }}
-          >
-            View History
-          </button>
-        </div>
-      </header>
+      {email && (
+        <header className={hstyles.header}>
+          <div>
+            <img className={styles.filtergreen} src="/logo.svg" alt="logo" />
+          </div>
+          <div className="flex space-x-4">
+            <span>{email}</span>
+            <button
+              className={hstyles.button}
+              onClick={() => {
+                navigate("/journal");
+              }}
+            >
+              New Entry
+            </button>
+            <button
+              className={hstyles.button}
+              onClick={() => {
+                navigate("/view-journal");
+              }}
+            >
+              View History
+            </button>
+          </div>
+        </header>
+      )}
     </>
   );
 }
