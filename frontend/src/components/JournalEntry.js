b848f906e5361afc3703d7e55b2a992e9bbbd206
adiplier <ag1482@rit.edu>
2025-02-23 01:11:42 -0500

    Journal entry css

--- frontend/src/components/JournalEntry.js
+++ frontend/src/components/JournalEntry.js
@@ -1,18 +1,22 @@
 import { useState } from "react";
+import "./pallette.css";
+import styles from "./Login.module.css";
+import jestyles from "./JournalEntry.module.css";
 
 function JournalEntry() {
   const [feeling, setFeeling] = useState("");
   const [mood, setMood] = useState("");
   const [sleepQuality, setSleepQuality] = useState(5);
   return (
-    <div>
-      <h1>Journal Entry for {new Date().toLocaleDateString()}</h1>
-      <form>
+    <div className={styles.container}>
+      <h1 className={styles.filtergreen}>Journal Entry for {new Date().toLocaleDateString()}</h1>
+      <form className={jestyles.form}>
         <div>
           <label>How are you feeling today?</label>{" "}
         </div>
         <div>
           <select
+            className={jestyles.dropdowninput}
             value={feeling}
             onChange={(e) => setFeeling(e.target.value)}
             id="options"
@@ -31,6 +35,7 @@ function JournalEntry() {
           </div>
           <div>
             <select
+              className={jestyles.dropdowninput}
               value={mood}
               onChange={(e) => setMood(e.target.value)}
               id="options"
@@ -50,9 +55,10 @@ function JournalEntry() {
           <label>Your journal entry for today:</label>
           <div>
             <textarea
+              className={jestyles.textarea}
               required
               placeholder="Start typing..."
-              rows="10"
+              rows="14"
               cols="50"
             ></textarea>
           </div>
@@ -63,6 +69,7 @@ function JournalEntry() {
               On a scale of 1-10, how well did you sleep last night?
             </label>
             <input
+            className={jestyles.slider}
               type="range"
               min="1"
               max="10"
@@ -75,7 +82,7 @@ function JournalEntry() {
         </div>
         <br />
         <div>
-          <button type="submit">Submit</button>
+          <button className={styles.button} type="submit">Submit</button>
         </div>
       </form>
     </div>
